a0b51329957f41ad6f52dc09c48231ed176ffe5f
Stephen Wai <swai@twilio.com>
2015-11-19 21:18:57 -0800

    Initial commit of onmyway

---
+++ index.js
@@ -0,0 +1,95 @@
+/*
+ * Stephen Wai
+ * index.js
+ */
+
+'use strict';
+
+var express = require('express');
+var envvar = require('envvar');
+var request = require('request');
+
+var TWILIO_ACCOUNT_SID = envvar.string('TWILIO_ACCOUNT_SID');
+var TWILIO_AUTH_TOKEN = envvar.string('TWILIO_AUTH_TOKEN');
+var TWILIO_NUMBER = envvar.string('TWILIO_NUMBER');
+
+var GOOGLE_MAP_KEY = envvar.string('GOOGLE_MAPS_KEY');
+
+var client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+var DEFAULT_SMS_RECIPIENT="+15555555";
+var DEFAULT_DESTINATION = "Embarcadero, San Francisco, CA";
+var DEFAULT_MODE = "driving";
+var MESSAGE_BODY = "On my way! ETA ";
+
+var app = express();
+app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/eta', function (req, res) {
+
+  // req parameters
+  var lat = req.query.lat;
+  var long = req.query.long;
+  var recipient = req.query.recipient || DEFAULT_SMS_RECIPIENT;
+  var destination = req.query.destination || DEFAULT_DESTINATION;
+  var mode = req.query.mode || DEFAULT_MODE;
+
+  // set params for Google Maps call
+  //var origins = "645 Harrison St, San Francisco, CA";
+  // https://developers.google.com/maps/documentation/distance-matrix/
+  var origins = lat + "," + long;
+  var googleUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + origins +
+                    "&destinations=" + destination +
+                    "&units=imperial" +
+                    "&mode=" + mode +
+                    "&key=" + GOOGLE_MAP_KEY;
+  var sms = MESSAGE_BODY;
+
+  // debug
+  console.log("Calling Google Maps API:")
+  console.log(googleUrl);
+
+  // call Google Maps API
+  request(googleUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var responseBody = JSON.parse(body);
+        var duration = responseBody["rows"][0]["elements"][0]["duration"]["text"];
+        var distance = responseBody["rows"][0]["elements"][0]["distance"]["text"];
+        var origin = responseBody["origin_addresses"][0];
+        sms += duration + " from " + origin;
+
+        console.log("Response:")
+        console.log(body);
+
+        // call Twilio API to text recipient
+        sendSMS(recipient, sms);
+
+        res.send(responseBody);
+      }
+  }) // end request call
+
+});
+
+function sendSMS(recipient, body) {
+  client.sendMessage({
+    to: recipient,
+    from: TWILIO_NUMBER,
+    body: body
+
+  }, function(error, message) {
+    if (!error) {
+      console.log('Success! The SID for this SMS message is:');
+      console.log(message.sid);
+
+      console.log('Message sent on:');
+      console.log(message.dateCreated);
+    } else {
+      console.log('Oops! There was an error.');
+    }
+  });
+}
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+});
---
+++ public/app.js
@@ -0,0 +1,102 @@
+/*
+ * Stephen Wai
+ * app.js
+ * 
+ */
+
+// magic function to get url params
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function getEta(recipient, destination, mode, pos) {
+  $.ajax({
+    url: "/eta",
+    type: 'GET',
+    data: { recipient: recipient,
+      destination: destination,
+      mode: mode,
+      lat: pos.lat,
+      long: pos.long
+    },
+    contentType: 'application/json; charset=utf-8',
+    success: function(data) {
+      console.log("Success making AJAX request to ETA");
+      //console.log(data);
+      //var responseBody = JSON.parse(data);
+      var responseBody = data;
+      var origin = responseBody["origin_addresses"][0];
+      var destination = responseBody["destination_addresses"][0];
+      var duration = responseBody["rows"][0]["elements"][0]["duration"]["text"];
+      var t = "origin: " + origin + " | destination: " + destination + " | duration: " + duration;
+      console.log(t);
+      $("#origin-out").text(origin);
+      $("#destination-out").text(destination);
+      $("#duration-out").text(duration);
+    },
+    error: function() {
+      console.log("Error making AJAX request to ETA");
+    }
+  }); // end $.ajax
+}
+
+// submit click handler
+$("#sendEta").click(function() {
+
+  // get recipient destination from form
+  var phone = $("#phone").val();
+  var destination = $("#destination").val();
+  var mode = $("#mode").val();
+  var queryParams = "?recipient=" + encodeURIComponent(phone)
+                    + "&destination=" + encodeURIComponent(destination)
+                    + "&mode=" + mode;
+
+  // reload page with params
+  var page = location.protocol + "//" + location.host + "/" + queryParams;
+  console.log("page: " + page);
+  window.location = page;
+
+});
+
+// load onready
+$(document).ready(function() {
+
+    // DOM object for geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        };
+
+        // get params from url if present
+        var recipient = getParameterByName("recipient");
+        var destination = getParameterByName("destination");
+        var mode = getParameterByName("mode");
+
+        // exit if no recipient, destination or long/lat
+        // TODO: more validation
+        if (!recipient || !destination || !pos)
+          return;
+
+        // fill out form with values
+        $("#phone").val(recipient);
+        $("#destination").val(destination);
+        $("#mode").val(mode);
+
+        // debug
+        console.log("Calling service on document.ready");
+        console.log("Coordinates, lat:" + pos.lat + " | long:" + pos.long);
+        console.log("Recipient: " + recipient);
+        console.log("Destination: " + destination);
+        console.log("Mode: " + mode);
+
+        // call AJAX
+        getEta(recipient, destination, mode, pos);
+
+    }); // end navigator.geolocation.getCurrentPosition
+  } // end if(navigator.geolocation) {
+}); // end document.ready
